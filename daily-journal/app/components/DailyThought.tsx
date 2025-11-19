0199852b506b74e92c241d73039401bc606ce029
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2025-11-19 13:57:50 -0600

    Add Thoughts
    Finished adding thoughts to the home page. Still need to save thought with localStorage.

--- daily-journal/app/components/DailyThought.tsx
+++ daily-journal/app/components/DailyThought.tsx
@@ -2,13 +2,56 @@
 import React, { useState } from "react";
 
 export default function DailyThought() {
+    // input is the variable and setInput updates the variable
+    const [input, setInput] = useState("");
+    const [thoughts, setThoughts] = 
+    useState<{ text: string; time: string }[]>([]);
+    
+    // Handle saving a thought
+    const handleSave = () => {
+        if (input.trim() === "") return;
+
+        // Create timestamp
+        const now = new Date();
+        const timestamp = now.toLocaleString("en-US", {
+            weekday: "short",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+
+        const newThought = { text: input, time: timestamp };
+        setThoughts([newThought, ...thoughts]);
+        setInput("");
+    }
+
     return (
         <div className="bg-[#ff0000] text-white p-6 rounded-xl shadow-md text-center max-w-md w-full">
             <h2 className="text-xl font-bold mb-3">Daily Thoughts</h2>
             <textarea 
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
                 placeholder="Type your thoughts for the day..."
-                className="w-full p-2 rounded-md focus:outline-none"
+                className="w-full p-2 rounded-md focus:outline-none resize-none"
             />
+            <button
+                onClick={handleSave}
+                className="mt-3 bg-white text-[#ff0000] px-4 py-2 rounded-md
+            font-semibold hover:bg-[#000000] transition-colors cursor-pointer">
+                Save Thought
+            </button>
+
+            <div className="mt-5 space-y-3 text-left">
+                {thoughts.length === 0 ? (<p className="italic text-center">No thoughts yet. Start typing!</p>) : (
+                    thoughts.map((thought, index) => (
+                        <div 
+                            key={index}
+                            className="bg-white/20 p-3 rounded-lg shadow-sm">
+                            <p className="text-lg">{thought.text}</p>
+                            <p className="text-sm opacity-80 mt-1">{thought.time}</p>    
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
     );
 }
