4e0420699418af72c6eee7d192cf96cec0f42391
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2025-12-02 13:32:14 -0600

    Started Entry Route
    Started to create entry route and moved database info to library file.

--- daily-journal/app/api/competencies/route.ts
+++ daily-journal/app/api/competencies/route.ts
@@ -1,20 +1,10 @@
 import { NextResponse } from "next/server";
-import mysql from "mysql2/promise";
+import connection from "@/app/lib/db";
 
 // A GET function returns some data based on a request
 export async function GET() {
     try {
-        // Connection returns a promise 
-        // (placeholder for the value that will eventually be returned)
-        const connection = await mysql.createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB,
-        });
-
         const [rows] = await connection.execute("SELECT id, skill, description FROM Competency ORDER BY skill ASC");
-
         return NextResponse.json(rows);
     }
     catch (err) {
